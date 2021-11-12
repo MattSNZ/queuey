920e8e707d2f2061be1e197f5e37cf523877d30c
Matthew Searle <matthewjsearle@gmail.com>
2021-11-12 15:11:35 +1300

    it loads! but does not refresh :/

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,16 @@
+let client
+let subdomain
+
 $(function () {
-  const client = ZAFClient.init()
+  client = ZAFClient.init()
   // client.invoke('resize', { width: '400px', height: '500px' })
-  requestViews(client)
-    // .then(() => {
-    // })
+  // bindEventListener()
+  client.context()  
+    .then(res => {
+      return subdomain = res.account.subdomain
+    })
+    .then(subdomain => requestViews(client, subdomain))
+    .then(() => bindEventListener())
 })
 
 function showInfo (data) {
@@ -24,17 +31,16 @@ function showError(response) {
   $("#content").html(html)
 }
 
-function requestViews(client) {
+function requestViews(client, subdomain) {
   var settings = {
     url: '/api/v2/views/active.json',
     type:'GET',
     dataType: 'json',
   }
-  let views = { views: [] }
 
   return client.request(settings)
     .then(response => {
-      return Promise.all(response.views.map(each => getViewCount(client, each)))
+      return Promise.all(response.views.map(each => getViewCount(client, each, subdomain)))
     })
     .then(res => showInfo({ views: [...res] }))
     .catch(error => {
@@ -43,19 +49,33 @@ function requestViews(client) {
     })
 }
 
-function getViewCount(client, viewObj) {
+function getViewCount(client, viewObj, subdomain) {
   const settings = {
     url: '/api/v2/views/' + viewObj.id + '/count.json',
     type:'GET',
     dataType: 'json',
   }
+
   return client.request(settings)
     .then(res => {
       viewObj.ticketCount = res.view_count.pretty
+      viewObj.link = 'https://'+ subdomain + '.zendesk.com/agent/filters/' + viewObj.id
+      // console.log(viewObj)
       return viewObj
     })
     .catch(error => {
       console.error(error)
       showError(error)
     })
+}
+
+function buttonRefresh() {
+  $('#content').html('<img src="./loading-buffering.gif" heighy/>')
+  console.log('click!')
+  return requestViews(client, subdomain)
+}
+
+function bindEventListener() {
+  document.getElementById('refresh-button').addEventListener('click', buttonRefresh)
+  console.log('listener bound!')
 }
